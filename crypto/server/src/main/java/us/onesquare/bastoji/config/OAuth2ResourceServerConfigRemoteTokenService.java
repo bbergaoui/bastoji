51af1d211c8bf692c47d87bbe63fec53cf6626c8
Bergaoui Bacem <bacem@arcturustech.com>
2018-06-05 09:57:11 +0100

    Add server config

---
+++ crypto/server/src/main/java/us/onesquare/bastoji/config/OAuth2ResourceServerConfigRemoteTokenService.java
@@ -0,0 +1,37 @@
+package us.onesquare.bastoji.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+//@Configuration
+//@EnableResourceServer
+public class OAuth2ResourceServerConfigRemoteTokenService extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(final HttpSecurity http) throws Exception {
+        // @formatter:off
+                http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                    .and()
+                    .authorizeRequests().anyRequest().authenticated()
+                        .and()
+                        .csrf()
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+        // @formatter:on                
+    }
+
+    @Primary
+    @Bean
+    public RemoteTokenServices tokenServices() {
+        final RemoteTokenServices tokenService = new RemoteTokenServices();
+        tokenService.setCheckTokenEndpointUrl("http://localhost:8081/spring-security-oauth-server/oauth/check_token");
+        tokenService.setClientId("fooClientIdPassword");
+        tokenService.setClientSecret("secret");
+        return tokenService;
+    }
+
+}
